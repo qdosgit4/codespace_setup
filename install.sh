d12d62299c97d2731a593dc093e22b5867e55dc7
qdosgit3 <228497977+qdosgit3@users.noreply.github.com>
2026-03-03 20:13:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 sudo DEBIAN_FRONTEND=noninteractive apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install emacs-nox tmux -y
+
+mkdir -p ~/.emacs.d
+echo "(require 'package)
+(add-to-list 'package-archives '(\"melpa\" . \"https://melpa.org/packages/\") t)
+(package-refresh-contents)" >> ~/.emacs.d/init.el
